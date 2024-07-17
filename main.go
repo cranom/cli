e204479c0a5a5316bfee23218f92d57458cc0fd2
jim-junior <jimjunior854@gmail.com>
2024-07-18 00:15:37 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 Cranom Techonologies info@cranom.com
+
+*/
+package main
+
+import "github.com/cranom/cli/cmd"
+
+func main() {
+	cmd.Execute()
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# CRANOM CLI
+
+Cranom CLI is a CLI Client to interact with Cranom Platform Services.
