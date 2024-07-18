e79c8a9d9959b1fdde1cbb78ff35e095f9a8e6fb
jim-junior <jimjunior854@gmail.com>
2024-07-18 16:45:09 +0300

    chore: Add Cranom CLI commands for login, logout, create, list, delete, update, download, stop, start, status, run, logs, and delete-software

--- cmd/root.go
+++ cmd/root.go
@@ -22,9 +22,179 @@ var rootCmd = &cobra.Command{
 	Learn More at: https://www.cranom.tech/plaform-tools/cli
 	`,
 	Version: "0.0.1",
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Welcome to Cranom CLI")
+		fmt.Println("Use cranom --help to see available commands")
+	},
+}
+
+var loginCmd = &cobra.Command{
+	Use:   "login",
+	Short: "Login to Cranom Platform",
+	Long: `Login to Cranom Platform using your Cranom Account.
+
+	Example:
+	cranom login --username <username> --token <token>
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Login to Cranom Platform")
+	},
+}
+
+var logoutCmd = &cobra.Command{
+	Use:   "logout",
+	Short: "Logout from Cranom Platform",
+	Long: `Logout from Cranom Platform using your Cranom Account.
+
+	Example:
+	cranom logout
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Logout from Cranom Platform")
+	},
+}
+
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Create a new resource on Cranom Platform",
+	Long: `Create a new resource on Cranom Platform using your Cranom Account.
+
+	Example:
+	cranom create <software> --name <name> --version <version>
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Create a new resource on Cranom Platform")
+	},
+}
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all resources on Cranom Platform",
+	Long: `List all resources on Cranom Platform using your Cranom Account.
+
+	Example:
+	cranom list <software>
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("List all resources on Cranom Platform")
+	},
+}
+
+var deleteCmd = &cobra.Command{
+	Use:   "delete",
+	Short: "Delete a resource on Cranom Platform",
+	Long: `Delete a resource on Cranom Platform using your Cranom Account.
+
+	Example:
+	cranom delete <software> --name <name> --version <version>
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Delete a resource on Cranom Platform")
+	},
+}
+
+var updateCmd = &cobra.Command{
+	Use:   "update",
+	Short: "Update a resource on Cranom Platform",
+	Long: `Update a resource on Cranom Platform using your Cranom Account.
+
+	Example:
+	cranom update <software> --name <name> --version <version>
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Update a resource on Cranom Platform")
+	},
+}
+
+var downloadCmd = &cobra.Command{
+	Use:   "download",
+	Short: "Download a resource from Cranom Platform",
+	Long: `Download a resource from Cranom Platform using your Cranom Account.
+
+	Example:
+	cranom download <software> --name <name> --version <version>
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Download a resource from Cranom Platform")
+	},
+}
+
+var stopCmd = &cobra.Command{
+	Use:   "stop",
+	Short: "Stop a resource on Cranom Platform",
+	Long: `Stop a resource on Cranom Platform using your Cranom Account.
+
+	Example:
+	cranom stop <software> --name <name> --version <version>
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Stop a resource on Cranom Platform")
+	},
+}
+
+var startCmd = &cobra.Command{
+	Use:   "start",
+	Short: "Start a resource on Cranom Platform",
+	Long: `Start a resource on Cranom Platform using your Cranom Account.
+
+	Example:
+	cranom start <software> --name <name> --version <version>
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Start a resource on Cranom Platform")
+	},
+}
+
+var statusCmd = &cobra.Command{
+	Use:   "status",
+	Short: "Get status of a resource on Cranom Platform",
+	Long: `Get status of a resource on Cranom Platform using your Cranom Account.
+
+	Example:
+	cranom status <software> --name <name> --version <version>
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Get status of a resource on Cranom Platform")
+	},
+}
+
+var runCmd = &cobra.Command{
+	Use:   "run",
+	Short: "Run a resource on Cranom Platform",
+	Long: `Run a resource on Cranom Platform using your Cranom Account.
+
+	Example:
+	cranom run <software> --name <name> --version <version>
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Run a resource on Cranom Platform")
+	},
+}
+
+var logsCmd = &cobra.Command{
+	Use:   "logs",
+	Short: "Get logs of a resource on Cranom Platform",
+	Long: `Get logs of a resource on Cranom Platform using your Cranom Account.
+
+	Example:
+	cranom logs <software> --name <name> --version <version>
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Get logs of a resource on Cranom Platform")
+	},
+}
+
+var deleteSoftwareCmd = &cobra.Command{
+	Use:   "delete-software",
+	Short: "Delete a software on Cranom Platform",
+	Long: `Delete a software on Cranom Platform using your Cranom Account.
+
+	Example:
+	cranom delete-software <software>
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Delete a software on Cranom Platform")
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -38,6 +208,20 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
+
+	rootCmd.AddCommand(loginCmd)
+	rootCmd.AddCommand(logoutCmd)
+	rootCmd.AddCommand(createCmd)
+	rootCmd.AddCommand(listCmd)
+	rootCmd.AddCommand(deleteCmd)
+	rootCmd.AddCommand(updateCmd)
+	rootCmd.AddCommand(downloadCmd)
+	rootCmd.AddCommand(stopCmd)
+	rootCmd.AddCommand(startCmd)
+	rootCmd.AddCommand(statusCmd)
+	rootCmd.AddCommand(runCmd)
+	rootCmd.AddCommand(logsCmd)
+	rootCmd.AddCommand(deleteSoftwareCmd)
 
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
